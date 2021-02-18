791c5ca5bb05c47df2b49c6c1436e600384f6e15
gabriel7mendes <andrewsseven@gmail.com>
2021-02-18 10:52:07 -0300

    first commit

---
+++ src/com/balazscholczer/udemy/Constants.java
@@ -0,0 +1,12 @@
+package com.balazscholczer.udemy;
+
+public class Constants {
+	
+	public Constants() {
+		
+	}
+	
+	public static final int NUMBER_OF_STUDENTS = 5;
+	public static final int NUMBER_OF_BOOKS = 7;
+
+}
